110001768b4e6d8fd18372d52cb4fa7be7b96648
xudong722 <xudong722@126.com>
2025-04-06 21:44:55 +0800

    timer imple -2

--- East/include/Timer.h
+++ East/include/Timer.h
@@ -2,7 +2,7 @@
  * @Author: Xudong0722 
  * @Date: 2025-04-05 20:22:11 
  * @Last Modified by: Xudong0722
- * @Last Modified time: 2025-04-06 16:53:35
+ * @Last Modified time: 2025-04-06 21:36:10
  */
 
 #include <functional>
@@ -19,11 +19,13 @@ class Timer : public std::enable_shared_from_this<Timer> {
   using sptr = std::shared_ptr<Timer>;
   Timer(uint64_t period, std::function<void()> cb, bool recurring,
         TimerManager* manager);
+  Timer(uint64_t execute_time);
 
  private:
-  bool m_recurring{false};             //是否是循环定时器
-  uint64_t m_period{0};                //定时器执行周期， 单位：ms
-  uint64_t m_execute_time{0};          //精确的执行时间
+  bool m_recurring{false};  //是否是循环定时器
+  uint64_t m_period{0};     //定时器执行周期， 单位：ms
+  uint64_t m_execute_time{
+      0};  //精确的执行时间, TODO这个含义可能不够准确，后续纠正
   std::function<void()> m_cb;          //定时器超时时回调函数
   TimerManager* m_timer_mgr{nullptr};  //当前timer所属的管理器
  private:
@@ -43,10 +45,19 @@ class TimerManager {
   TimerManager();
   virtual ~TimerManager();
 
+  //添加定时器任务
   void addTimer(uint64_t period, std::function<void()> cb,
                 bool recurring = false);
+
+  //添加带有条件的定时器任务
   void addConditionTimer(uint64_t period, std::function<void()> cb,
                          std::weak_ptr<void> weak_cond, bool recurring = false);
+
+  //定时器队列头部的定时器执行时间与当前时间的间隔
+  uint64_t getNextTimer();
+
+  //获取所有已经超时的定时器的回调函数
+  void listExpiredCb(std::vector<std::function<void()>>& cbs);
 
  protected:
   virtual void onTimerInsertAtFront() = 0;
--- East/src/Timer.cc
+++ East/src/Timer.cc
@@ -2,10 +2,11 @@
  * @Author: Xudong0722 
  * @Date: 2025-04-05 20:22:15 
  * @Last Modified by: Xudong0722
- * @Last Modified time: 2025-04-06 16:54:47
+ * @Last Modified time: 2025-04-06 21:44:30
  */
 
 #include "Timer.h"
+#include <algorithm>
 #include "util.h"
 
 namespace East {
@@ -16,6 +17,8 @@ Timer::Timer(uint64_t period, std::function<void()> cb, bool recurring,
   m_execute_time = GetCurrentTimeInMs() + m_period;
 }
 
+Timer::Timer(uint64_t execute_time) : m_execute_time(execute_time) {}
+
 bool Timer::Cmp::operator()(const Timer::sptr& lhs,
                             const Timer::sptr& rhs) const {
   if (nullptr == lhs && nullptr == rhs)
@@ -24,10 +27,13 @@ bool Timer::Cmp::operator()(const Timer::sptr& lhs,
     return true;
   if (nullptr == rhs)
     return false;
+  if (lhs->m_execute_time == rhs->m_execute_time) {
+    return lhs < rhs;
+  }
   return lhs->m_execute_time < rhs->m_execute_time;
 }
 
-TimerManager::TimerManager(){} 
+TimerManager::TimerManager() {}
 TimerManager::~TimerManager() {}
 
 void TimerManager::addTimer(uint64_t period, std::function<void()> cb,
@@ -35,7 +41,7 @@ void TimerManager::addTimer(uint64_t period, std::function<void()> cb,
   Timer::sptr timer = std::make_shared<Timer>(period, cb, recurring, this);
   bool at_front{false};
   {
-    RWMutexType::WLockGuard lock(m_mutex);
+    RWMutexType::WLockGuard wlock(m_mutex);  //TODO，是否可以放在{}里？
     auto it = m_timers.insert(timer).first;
     if (it == m_timers.begin()) {
       at_front = true;
@@ -49,5 +55,65 @@ void TimerManager::addTimer(uint64_t period, std::function<void()> cb,
 
 void TimerManager::addConditionTimer(uint64_t period, std::function<void()> cb,
                                      std::weak_ptr<void> weak_cond,
-                                     bool recurring) {}
+                                     bool recurring) {
+  auto func = [weak_cond, cb]() -> void {
+    auto tmp = weak_cond.lock();
+    if (tmp != nullptr) {
+      cb();
+    }
+  };
+
+  return addTimer(period, func, recurring);
+}
+
+uint64_t TimerManager::getNextTimer() {
+  RWMutexType::RLockGuard rlock(m_mutex);
+  if (m_timers.empty()) {
+    return ~0ull;
+  }
+
+  const Timer::sptr& next = *m_timers.begin();
+  const uint64_t now_ms = GetCurrentTimeInMs();
+  if (now_ms >= next->m_execute_time) {
+    return 0ull;
+  }
+
+  return next->m_execute_time - now_ms;
+}
+
+void TimerManager::listExpiredCb(std::vector<std::function<void()>>& cbs) {
+  const uint64_t now_ms = GetCurrentTimeInMs();
+  {
+    RWLock::RLockGuard rlock(m_mutex);
+    if (m_timers.empty()) {
+      return;
+    }
+  }
+
+  RWLock::WLockGuard wlock(m_mutex);
+  if (m_timers.empty()) {
+    return;
+  }
+
+  Timer::sptr timer_for_now = std::make_shared<Timer>(now_ms);
+  auto it = std::lower_bound(begin(m_timers), end(m_timers), timer_for_now);
+  //lower_bound是找到第一个不小于timer_for_now的元素，我们还得加上等于timer_for_now的
+  while (it != m_timers.end() && (*it)->m_execute_time == now_ms) {
+    ++it;
+  }
+
+  cbs.clear();
+  std::vector<Timer::sptr> expired(m_timers.begin(), it);
+  cbs.resize(expired.size());
+  m_timers.erase(m_timers.begin(), it);
+  auto i = 0u;
+  for (const auto& p : expired) {
+    cbs[i] = p->m_cb;
+    if (p->m_recurring) {
+      p->m_execute_time += p->m_period;
+      m_timers.insert(p);
+    }
+  }
+}
+
 }  // namespace East
